66968689ae9e1b788a85983ef97ee398206d1def
Edward Amartey <87177605+EdJnr@users.noreply.github.com>
2024-05-21 16:48:59 +0000

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import { mainBrandName } from "../info/genInfo"
 export const Footer=()=>{
     return(
         <div className=" py-14">
-            <p className=" text-sm md:text-base text-gray-400">👏 Developed By Edward</p>
-            <p className=" text-sm md:text-base text-gray-400">{mainBrandName} 2023. All rights reserved</p>
+            <p className=" text-sm md:text-base text-gray-400">👏 Developed By Bernard Okine</p>
+            <p className=" text-sm md:text-base text-gray-400">{mainBrandName} 2024. All rights reserved</p>
         </div>
     )
 }
